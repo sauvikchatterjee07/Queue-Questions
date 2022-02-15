834ceb79a318e47fce8bc22d405a5b2ddef988a5
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-15 20:28:45 +0530

    Update 232. Implement Queue using Stacks.cpp

--- 232. Implement Queue using Stacks.cpp
+++ 232. Implement Queue using Stacks.cpp
@@ -1,3 +1,12 @@
+    
+
+
+
+    Push Operation is O(N)
+
+
+
+
     MyQueue() {
         
     }
@@ -30,3 +39,62 @@
     bool empty() {
         return s1.empty();
     }
+
+
+
+    Pop & Top Operation is O(1) amortise
+    
+    
+    
+    class MyQueue {
+public:
+    MyQueue() {
+        
+    }
+    stack<int> ip, op;
+
+    void push(int x) {
+        ip.push(x);
+    }
+    
+    int pop() {
+        int res = 0;
+        if(!op.empty()){
+            res = op.top();
+            op.pop();
+        }
+        else{
+            while(!ip.empty()){
+                op.push(ip.top());
+                ip.pop();
+            }
+            res = op.top();
+            op.pop();
+        }
+        return res;
+    }
+    
+    int peek() {
+        int res = 0;
+        if(!op.empty()){
+            res = op.top();
+        }
+        else{
+            while(!ip.empty()){
+                op.push(ip.top());
+                ip.pop();
+            }
+            res = op.top();
+        }
+        return res;
+    }
+    
+    bool empty() {
+        return op.empty() && ip.empty();
+    }
+};
+
+
+
+
+
